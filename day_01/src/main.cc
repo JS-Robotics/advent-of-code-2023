ac79687b02fc45d6e7cffd1be4b49e9e55164398
Sondre <sjohnsen.valle@gmail.com>
2023-12-01 17:23:52 +0100

    day 01 part one completed

--- day_01/src/main.cc
+++ day_01/src/main.cc
@@ -4,7 +4,18 @@
 
 
 #include <iostream>
+#include <string>
 #include <fstream>
+#include <vector>
+
+template<typename T>
+struct VectorTwo {
+  T first;
+  T second;
+
+  // Constructor that initializes first and second
+  VectorTwo(T first_init, T second_init) : first(first_init), second(second_init) {}
+};
 
 std::ifstream OpenFile(const std::string &file_name) {
   std::ifstream file(file_name);
@@ -19,19 +30,56 @@ void CloseFile(std::ifstream &file) {
   file.close();
 }
 
-void ReadAndPrintLines(std::ifstream &file) {
-  std::string line;
-  while (std::getline(file, line)) {
-    std::cout << line << std::endl;
+int CharToInt(char c) {
+  return c - '0';
+}
+
+VectorTwo<int> GetNumbersFromString(const std::string &string) {
+  std::vector<int> numbers;
+  char max_num_as_char = '9';
+  char min_num_as_char = '0';
+
+  for (char c : string) {
+    if (min_num_as_char <= c && c <= max_num_as_char) {
+      numbers.emplace_back(static_cast<int>(CharToInt(c)));
+    }
   }
+
+  if (numbers.size() == 1) {
+    return VectorTwo<int>{numbers.at(0), numbers.at(0)};
+  } else {
+    return VectorTwo<int>{numbers.at(0), numbers.at(numbers.size()-1)};
+  }
+}
+
+int GetValue(VectorTwo<int> &the_vector) {
+  int prem_val = the_vector.first * 10 + the_vector.second;  // TODO make better code for this
+  return prem_val;
+}
+
+int ReadAndPrintLines(std::ifstream &file) {
+  std::string line;
+  std::vector<VectorTwo<int>> extracted_numbers;
+  while (std::getline(file, line)) {
+    VectorTwo<int> vector_two = GetNumbersFromString(line);
+    extracted_numbers.emplace_back(vector_two);
+  }
+
+  int sum = 0;
+  for (auto number : extracted_numbers) {
+    sum += GetValue(number);
+  }
+
+  return sum;
 }
 
 int main() {
   std::cout << "Hello AoC Day 1! \n" << std::endl;
 
-  const char *file_name = "../data/day_01_example.txt";
+  const char *file_name = "../data/day_01_input.txt";
   std::ifstream file = OpenFile(file_name);
-  ReadAndPrintLines(file);
+  int calibration_sum = ReadAndPrintLines(file);
+  std::cout << "The calibration sum is: " << calibration_sum << std::endl;
   CloseFile(file);
   return 0;
 
