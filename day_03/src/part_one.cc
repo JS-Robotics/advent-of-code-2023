e730a0b96edc0c1fc2790926780e7dc3e93deb74
sondre <sondre.johnsen@hiveautonomy.no>
2023-12-04 11:25:27 +0100

    Started getting out concerning line sets

--- day_03/src/part_one.cc
+++ day_03/src/part_one.cc
@@ -25,9 +25,36 @@ void CloseFile(std::ifstream &file) {
 int ReadAndProcessGames(std::ifstream &file) {
   std::string line;
   int sum = 0;
-
+  std::vector<std::string> input;
+  std::vector<std::string> string_set;
+  std::string empty_line = "..........";
+  string_set.reserve(3);
+  int index = 0;
   while (std::getline(file, line)) {
     std::cout << line << std::endl;
+    input.emplace_back(line);
+  }
+
+  for (int i = 0; i < (input.size() - 1); i++) {
+    if (i == 0) {
+      string_set[0] = input.at(i);
+      string_set[1] = input.at(i + 1);
+      string_set[2] = empty_line;
+    } else if (i == input.size() - 1) {
+      string_set[0] = input.at(i - 1);
+      string_set[1] = input.at(i);
+      string_set[2] = empty_line;
+    } else {
+      string_set[0] = input.at(i - 1);
+      string_set[1] = input.at(i);
+      string_set[2] = input.at(i + 1);
+    }
+
+    std::cout  << std::endl;
+    std::cout << string_set[0] << std::endl;
+    std::cout << string_set[1] << std::endl;
+    std::cout << string_set[2] << std::endl;
+    std::cout << std::endl;
   }
 
   return sum;
@@ -46,7 +73,7 @@ int main() {
   const char *file_name = "../data/day_03_example.txt";
   std::ifstream file = OpenFile(file_name);
   int calibration_sum = ReadAndProcessGames(file);
-  std::cout << "The sum of valid game IDs are: \033[32m" << calibration_sum << "\033[0m" << std::endl;
+  std::cout << "The engine schematic sum is: \033[32m" << calibration_sum << "\033[0m" << std::endl;
   CloseFile(file);
   return 0;
 
