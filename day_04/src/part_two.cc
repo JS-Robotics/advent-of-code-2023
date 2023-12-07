3e4b8de30b4d148abd05c4b296b2752a8fe2801e
Sondre <sjohnsen.valle@gmail.com>
2023-12-07 22:46:38 +0100

    Completely stuck at day 4 part two

--- day_04/src/part_two.cc
+++ day_04/src/part_two.cc
@@ -9,6 +9,13 @@
 #include <vector>
 #include <regex>
 
+struct CardSet {
+  int card{};
+  int matches{};
+  std::vector<int> card_numbers{};
+  std::vector<int> winning_numbers{};
+};
+
 std::ifstream OpenFile(const std::string &file_name) {
   std::ifstream file(file_name);
   if (!file.is_open()) {
@@ -82,41 +89,83 @@ int ReadAndProcess(std::ifstream &file) {
   std::string line;
   std::vector<int> card_numbers;
   std::vector<int> winning_numbers;
+  std::vector<CardSet> card_sets;
+  std::vector<std::string> input;
 
-  int card = 1;
+  //Store game data into a vector of strings to process afterward
   while (std::getline(file, line)) {
-    card_numbers = GetCards(line);
-    winning_numbers = GetWinningNumbers(line);
-    int matches = 0;
+    input.emplace_back(line);
+  }
 
-    for (int number : card_numbers) {
-      for (int winning_number : winning_numbers) {
+  //Store all Cards and their data
+  int card = 1;
+  for (const std::string &game : input) {
+    CardSet card_set;
+    card_set.card = card;
+    card_set.card_numbers = GetCards(game);
+    card_set.winning_numbers = GetWinningNumbers(game);
+    card_sets.emplace_back(card_set);
+    card++;
+  }
+
+
+  //Store all matches for a card and winning numbers
+  for (CardSet &kCardSet : card_sets) {
+    std::string string = std::to_string(kCardSet.card) + " : ";
+    for (int number : kCardSet.card_numbers) {
+      string += " - " + std::to_string(number);
+    }
+    std::cout << string << std::endl;
+
+    string = std::to_string(kCardSet.card) + " : ";
+    for (int number : kCardSet.winning_numbers) {
+      string += " - " + std::to_string(number);
+    }
+    std::cout << string << std::endl;
+
+    int matches = 0;
+    for (int number : kCardSet.card_numbers) {
+      for (int winning_number : kCardSet.winning_numbers) {
         if (number == winning_number) {
           matches++;
         }
       }
+
     }
-    // 1: 2,3,4,5 - 4
-    // 2: 3,4 - 2
-    // o2: 3,4 - 2
-    // 3: 4,5 - 2
-    // 3: 4,5 - 2
-    // 3: 4,5 - 2
-    // o3: 4,5 - 2
-    //
-    int sum_part = 0;
-    if (matches > 0) {
-      for (int i = 1; i <= matches; i++) {
-        if (i == 1) {
-          sum_part = 1;
-        } else {
-          sum_part = sum_part * 2;
+    kCardSet.matches = matches;
+    std::cout << kCardSet.card << " : " << kCardSet.matches << std::endl;
+  }
+
+  std::vector<CardSet> total_cards = card_sets;
+  std::vector<CardSet> added_cards = card_sets;
+
+  bool new_card = true;
+  while (new_card) {
+    std::vector<CardSet> prelim_cards = std::move(added_cards);
+    added_cards.clear();
+    new_card = false;
+    for (CardSet &kCardSet : prelim_cards) {
+      if (kCardSet.matches > 0) {
+        new_card = true;
+        std::cout << "at: " << kCardSet.card << std::endl;
+        for (int i = kCardSet.card + 1; i < kCardSet.card +1+ kCardSet.matches; i++) {
+          std::cout << "adding: " << i << std::endl;
+          added_cards.emplace_back(card_sets.at(i));
         }
       }
-      sum += sum_part;
     }
+    for (const CardSet &kCardSet : added_cards) {
+      total_cards.emplace_back(kCardSet);
+    }
+  }
 
-    card++;
+  for (const CardSet &card_set_1 : added_cards) {
+    std::cout << card_set_1.card << std::endl;
+  }
+
+  // Print the modified card_sets
+  for (const auto &card_set : total_cards) {
+    std::cout << "Card: " << card_set.card << ", Matches: " << card_set.matches << std::endl;
   }
 
   return sum;
@@ -125,7 +174,7 @@ int ReadAndProcess(std::ifstream &file) {
 int main() {
   std::cout << "Hello AoC Day 4! \n" << std::endl;
 
-  const char *file_name = "../data/day_04_input.txt";
+  const char *file_name = "../data/day_04_example.txt";
   std::ifstream file = OpenFile(file_name);
   int calibration_sum = ReadAndProcess(file);
   std::cout << "The cards total sum is: \033[32m" << calibration_sum << "\033[0m" << std::endl;
